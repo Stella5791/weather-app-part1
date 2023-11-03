7c07aafd4ccc341e4c0274bc6b2185650cf0e329
Stella5791 <142344371+Stella5791@users.noreply.github.com>
2023-11-03 17:30:03 -0300

    added refresh weather function

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+}
+function searchCity(city) {
+  let apiKey = "c3a184e34eb028a0c58e461odf7dt8e2";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchCity.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
